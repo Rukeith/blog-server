61efccfd934d30b06899a31085d15f3420e67947
Rukeith <fenglan.crazydance@gmail.com>
2017-12-03 00:42:53 +0800

    Update coding style

--- model/init.js
+++ model/init.js
@@ -3,15 +3,7 @@ const config = require('../config.js');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUrl, config.mongoConfig).then(
-  async () => {
-    console.info('MongoDB connection successful !');
-    // const Tag = require('./tag.js');
-    // const tag = new Tag();
-    // const kk = await tag.create(`test`);
-    // console.log(`kk =`, kk);
-    // const back = await tag.findById(kk._id);
-    // // console.log(`back =`, back);
-  },
+  () => console.info('MongoDB connection successful !'),
   err => console.error('MongoDB connection fail :', err),
 );
 
