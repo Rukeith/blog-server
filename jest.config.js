88eac56405f6cf7053d5272376a73f487b637194
Rukeith <fenglan.crazydance@gmail.com>
2017-12-04 23:55:01 +0800

    Add jest configuartion file and demo test

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  bail: true, // Stop test when first test fail
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js}'],
+  coverageDirectory: '/public/coverage',
+  coveragePathIgnorePatterns: [
+    '<rootDir>/views/',
+    '<rootDir>/config/',
+    '<rootDir>/public/',
+    '<rootDir>/locales/',
+    '<rootDir>/node_modules/',
+  ],
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      branches: 100,
+      functions: 100,
+      statements: 100,
+    },
+  },
+  notify: true,
+  clearMocks: true,
+  resetMocks: true,
+  resetModules: true,
+  testEnvironment: 'node',
+  verbose: true,
+  watchPathIgnorePatterns: [
+    '<rootDir>/LICENSE',
+    '<rootDir>/.eslintrc',
+    '<rootDir>/.gitignore',
+    '<rootDir>/.eslintignore',
+    '<rootDir>/node_modules/',
+  ],
+};
