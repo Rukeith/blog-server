1d670ae45a57c487ca6e8738c67301ac93e5a040
Rukeith <fenglan.crazydance@gmail.com>
2018-03-06 17:56:01 +0800

    Refactor syntax

--- model/init.js
+++ model/init.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 /* istanbul ignore next */
-const mongoUrl = (process.env.MONGODB_URI) ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/rukeith-blog';
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rukeith-blog';
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoUrl).then(
   () => console.info('MongoDB connection successful !'),
